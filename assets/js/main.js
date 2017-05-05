7135363160661c19873e1bfb733cdb0df24cb153
valentina lopez <valentina lopez>
2017-05-05 02:28:01 -0300

    Crear columnas en Html

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function Squad(nombre,apellido,edad,hobbies){
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.edad = edad;
+	this.hobbies = hobbies;
+}
+
+var gilia = new Squad("Gilia","Góngora",28, "Escuchar música")
